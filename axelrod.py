d726d5370c9d754762e641054f92bae63fe2735c
Andrew Sims <acsims@tuta.io>
2019-02-09 11:20:29 +1100

    Include bootstrap styles

--- axelrod.py
+++ axelrod.py
@@ -1,44 +1,41 @@
 """
 Implementation of the Axelrod model of cultural convergence and polarisation
 
-AXELROD R (1997) The dissemination of culture - A model with local convergence and global polarization. 
-    Journal of Conflict Resolution 41(2), pp. 203-226.
+AXELROD R (1997) The dissemination of culture - A model with local convergence
+and global polarization. Journal of Conflict Resolution 41(2), pp. 203-226.
 
-TODO: make it faster with numpy and other optimisation libraries
-TODO: improve visualisation aesthetic
-TODO: allow realtime visualisation and not just start- and end-state visualisation
-TODO: implement tools for analysis
+TODO: Step by step visualisation
+TODO: Plugin to browser
+TODO: Ability to manipulate constants
+
 """
 
-# Import libraries
 import random as rd
 
-# Define constants
-SIZE = 5
-TRAITS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+SIZE = 10
+NUM_TRAITS = 10
+TRAITS = [i for i in range(0, NUM_TRAITS - 1)]
 FEATURES = 5
 RUNS = 1000
-
 SEED = 1
 
-# Seed pseudo-random number generator
 rd.seed(SEED)
 
-# Helpful functions for a square grid
+# Helper functions for a square grid
 def find_location(index, SIZE):
     """
     Finds the x and y coordinates in a square grid of SIZE**2 given an index.
     It is assumed that we are counting from left to right on a set of rows top-down, and that area is square.
     Rows and columns start from 0 given Python indexing conventions.
-    
+
     e.g.
         0  1  2
     0  [0, 1, 2,
     1   3, 4, 5,
     2   6, 7, 8]
-    
+
     4 = [1, 1]
-    
+
     """
     x_pos = index % SIZE
     y_pos = index // SIZE
@@ -65,31 +62,30 @@ def find_neighbors(loc):
     SE = loc[0] + 1, loc[1] + 1
     return [N, S, W, E, NW, NE, SW, SE]
 
-# Define the model and the agents as classes
 class Agent():
-    "Agents to populate the model"
-    
+    "Agent class to populate the model"
+
     def __init__(self):
         self.culture = [rd.choice(TRAITS) for i in range(FEATURES)]
-    
+
     def culture_share(self, target, model):
         similarity = 0
         for i in model.agents[target].culture:
             if i == self.culture[model.agents[target].culture.index(i)]:
                 similarity += 1
-                
+
         interaction_probability = similarity / FEATURES
-        
+
         if rd.uniform(0, 1) < interaction_probability:
             shared = rd.randint(0, FEATURES - 1)
             model.agents[target].culture[shared] = self.culture[shared]
 
 class Axelrod():
-    "This is the model"
-    
+    "Model class"
+
     def __init__(self):
         self.agents = [Agent() for i in range(SIZE**2)]
-    
+
     def tick(self):
         "Runs a single cycle of the simulation"
         try:
@@ -98,7 +94,7 @@ class Axelrod():
             active.culture_share(passive, self)
         except IndexError:
             self.tick()
- 
+
     def show_state(self):
         for n in range(0, SIZE):
             row = [self.agents[i].culture for i in range(n * SIZE, n * (SIZE) + SIZE)]
@@ -108,7 +104,7 @@ class Axelrod():
                 print("]", end="")
             print()
             print("-" * (SIZE*FEATURES*2+SIZE))
-    
+
     def run_sim(self):
         print("Initial state of the model:")
         print()
@@ -119,7 +115,9 @@ class Axelrod():
             self.tick()
         print("Final state of the model:")
         self.show_state()
-        
-model = Axelrod()
 
-model.run_sim()
+class Recorder():
+    "Record states of a model which includes it"
+
+    def __init__(self):
+        
